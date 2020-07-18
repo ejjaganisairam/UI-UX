aa33bf8ce3d850cd0494cd0d394ed379baedc137
Sairam.ejjagani <sairam.ejjagani@levadata.com>
2020-07-18 12:50:24 +0530

    paint

--- JS/index.js
+++ JS/index.js
@@ -2,3 +2,18 @@ function function3() {
    var myobj = document.getElementById("fun");
    myobj.remove();
 }
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
